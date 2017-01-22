abfc8a56ed68919c1baaef592bc8f9e9189ca59c
sean <shanyilei013@163.com>
2017-01-22 17:50:51 +0800

    add pipetest

---
+++ pipetest/piptest.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE 40
+
+int main()
+{
+    int n = 0;
+    int fd[2];
+
+    pid_t pid;
+    char line[MAX_LINE];
+    memset(line, 0, MAX_LINE);
+
+    char *psz = "hello world!";
+    
+    if (pipe(fd) < 0)
+    {
+        printf("pipe error!\n");
+        exit(-1);
+    }
+    if ((pid = fork()) < 0)
+    {
+        printf("fork error!\n");
+        exit(-2);
+    }
+    else if (pid > 0)
+    {
+        close(fd[0]);
+        write(fd[1], psz, strlen(psz));
+    }
+    else
+    {
+        close(fd[1]);
+        n = read(fd[0], line, MAX_LINE);
+        write(STDOUT_FILENO, line, n);
+        exit(0);
+    }
+    
+    return 0;
+}
