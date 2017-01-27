9a128b6237fbcf72742bd7dcdd97663e63f95737
sean <shanyilei013@163.com>
2017-01-28 01:26:42 +0800

    modify execlp failed

--- pipetest/pipetest2.c
+++ pipetest/pipetest2.c
@@ -22,21 +22,20 @@ int main(int argc, char** argv)
     {
         close(fd[1]);
         dup2(fd[0], 0);
-        sleep(3);
-        if (execlp("wc", "-l", (char*)0) < 0)
+        if (execlp("wc", "wc", "-l", (char*)0) < 0)
         {
             printf("father execl failed!\n");
         }
     }
     else if (pid == 0)
     {
+        sleep(5);
         close(fd[0]);
         dup2(fd[1], 1);
-        if (execlp("ls", "-l", (char*)0) < 0)
+        if (execlp("ls", "ls", "-l", (char*)0) < 0)
         {
             printf("child execl fail\n");
         }
-        sleep(2);
     }
     return 0;
 }
