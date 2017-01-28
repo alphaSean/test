b26896a8b65d4e3eb08879f2fdad0843e7093776
sean <shanyilei013@163.com>
2017-01-29 02:36:29 +0800

    modify pipetest3

--- pipetest/pipetest3.c
+++ pipetest/pipetest3.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <sys/wait.h>
 
 #define MAX_BUF_SIZE 256
-int main()
+#define DEFAULT_PAGER "/bin/more"
+int main(int argc, char** argv)
 {
     pid_t pid;
     int fd[2];
     char buf[MAX_BUF_SIZE];
     FILE* pFile = NULL;
+    char *pager = NULL;
+    char *argv0 = NULL;
+    
+    if ((pFile = fopen(argv[1], "r")) < 0)
+    {
+        printf("can not open %s\n", argv[1]);
+        return -1;
+    }
+
     if (pipe(fd) < 0)
     {
         printf("pipe failed!\n");
@@ -33,20 +44,37 @@ int main()
             }
             close(fd[0]);
         }
-        
-        if (execl("/bin/cat", "cat", (char*)0) < 0)
+        if ((pager = getenv("PAGER")) == NULL)
         {
-            printf("father execl failed!\n");
+            pager = DEFAULT_PAGER;
+        }
+
+        argv0 = strrchr(pager, '/');
+        if(argv0)
+        {
+            argv0++;
+        }
+        else
+        {
+            argv0 = pager;
+        }
+        
+        if (execl(pager, argv0, NULL) < 0)
+        {
+            printf("child execl failed!\n");
         }
     }
     else
     {
         close(fd[0]);
-        pFile = fopen("/home/sean/test.txt", "r");
 
         while (fgets(buf, MAX_BUF_SIZE, pFile))
         {
             write(fd[1], buf, strlen(buf));
+        }
+        if (ferror(pFile))
+        {
+            printf("fgets error!\n");
         }
 
         if (waitpid(pid, NULL, 0) == -1)
